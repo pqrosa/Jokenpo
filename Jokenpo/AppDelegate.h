51c087f181b3bbf50ac7b5b18422cb77ebc2f53a
pqrosa <pqrosa@o2c.tv.br>
2013-03-20 17:46:27 -0300

    Primeiro commit.

---
+++ Jokenpo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Jokenpo
+//
+//  Created by Philippe Rosa on 3/20/13.
+//  Copyright (c) 2013 O2C Hipermídia. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Jokenpo/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  Jokenpo
+//
+//  Created by Philippe Rosa on 3/20/13.
+//  Copyright (c) 2013 O2C Hipermídia. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
